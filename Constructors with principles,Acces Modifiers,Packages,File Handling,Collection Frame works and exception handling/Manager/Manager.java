0a37650c7d698d27920d2a724e5367b0e551c269
TanniruSahithi <tannirusahithi3@gmail.com>
2024-06-16 13:44:30 +0530

    Add files via upload

---
+++ Constructors with principles,Acces Modifiers,Packages,File Handling,Collection Frame works and exception handling/Manager/Manager.java
@@ -0,0 +1,18 @@
+package constructors.manager;
+
+public class Manager {
+	private String name;
+    private int age;
+	public Manager(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+}
