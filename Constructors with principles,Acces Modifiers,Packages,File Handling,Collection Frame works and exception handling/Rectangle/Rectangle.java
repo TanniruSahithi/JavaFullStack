c1d7fa543514ef2bae533cf313bfcab73de77939
TanniruSahithi <tannirusahithi3@gmail.com>
2024-06-16 13:49:37 +0530

    Add files via upload

---
+++ Constructors with principles,Acces Modifiers,Packages,File Handling,Collection Frame works and exception handling/Rectangle/Rectangle.java
@@ -0,0 +1,30 @@
+package constructors.rectangle;
+
+public class Rectangle {
+	private double length;
+    private double width;
+    public double calculateArea() {
+        return length * width;
+    }
+ public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	public Rectangle(double length, double width) {
+		super();
+		this.length = length;
+		this.width = width;
+	}
+
+}
