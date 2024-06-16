30b0a38bc8717a04d7c65e7fcad0ed003b140dac
TanniruSahithi <tannirusahithi3@gmail.com>
2024-06-16 13:48:47 +0530

    Add files via upload

---
+++ Constructors with principles,Acces Modifiers,Packages,File Handling,Collection Frame works and exception handling/person/Person.java
@@ -0,0 +1,26 @@
+package constructors.person;
+
+public class Person {
+	private String name;
+    protected int age;
+    String address; // Default access modifier (package-private)
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
+
